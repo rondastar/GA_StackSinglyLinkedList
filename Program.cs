6a15271bfc3b23c2095a2e757da346016b214f13
rondastar <116147793+rondastar@users.noreply.github.com>
2024-02-08 21:28:34 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GA_StackSinglyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_StackSinglyLinkedList
+{
+    internal class Stack<T>
+    {
+        StackNode<T> top;
+        int count;
+
+        public Stack()
+        {
+            top = null;
+            count = 0;
+        } // Stack constructor
+
+        internal class StackNode<T> // Internal class for the nodes of the stack
+        {
+            T Value;
+
+            public StackNode(T value)   // creates a node with the given value
+            {
+                Value = value;
+            } // StackNode constructor
+
+        } // StackNode nested class
+
+    } // Stack class
+}
