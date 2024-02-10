dfdad5b094e5f4d0a974e6f2637940d524fe0e43
rondastar <116147793+rondastar@users.noreply.github.com>
2024-02-09 20:04:18 -0800

    Created and tested all methods

--- Program.cs
+++ Program.cs
@@ -1,10 +1,101 @@
-﻿namespace GA_StackSinglyLinkedList
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace GA_StackSinglyLinkedList
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            FloatTest();
+            //StringTest();
+        }
+
+        static void StringTest()
+        {
+            // create a new stack
+            Stack<string> stack = new Stack<string>();
+
+            // test push
+            stack.Push("apricot");
+            stack.Push("banana");
+            stack.Push("coconut");
+            stack.Push("durian");
+            stack.Push("eggplant");
+
+            // test count
+            Console.WriteLine(stack.Count); // 5
+
+            // test pop
+            stack.Pop();
+
+            // test peek
+            Console.WriteLine(stack.Peek()); // durian
+
+            // test clear
+            stack.Clear();
+
+            // test count
+            Console.WriteLine(stack.Count); // 0
+
+            // test peek
+            Console.WriteLine(stack.Peek()); // blank
+
+            // test pop
+            Console.WriteLine(stack.Pop()); // blank
+
+            // test push
+            stack.Push("apple");
+
+            // test pop
+            Console.WriteLine(stack.Pop()); // apple
+
+            // test peek
+            Console.WriteLine(stack.Peek()); // blank
+
+        } // StringTest
+
+        static void FloatTest()
+        {
+            // create a new stack
+            Stack<float> stack = new Stack<float>();
+
+            // test push
+            stack.Push(1.2F);
+            stack.Push(2.3F);
+            stack.Push(3.4F);
+            stack.Push(4.4F);
+            stack.Push(5.6F);
+
+            // test count
+            Console.WriteLine(stack.Count); // 5
+
+            // test pop
+            stack.Pop();
+
+            // test peek
+            Console.WriteLine(stack.Peek()); // 4.4
+
+            // test clear
+            stack.Clear();
+
+            // test count
+            Console.WriteLine(stack.Count); // 0
+
+            // test peek
+            Console.WriteLine(stack.Peek()); // 0
+
+            // test pop
+            Console.WriteLine(stack.Pop()); // 0
+
+            // test push
+            stack.Push(7.5F);
+
+            // test pop
+            Console.WriteLine(stack.Pop()); // 7.5
+
+            // test peek
+            Console.WriteLine(stack.Peek()); // 0
+
         }
     }
 }
--- Stack.cs
+++ Stack.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,16 +18,104 @@ namespace GA_StackSinglyLinkedList
             count = 0;
         } // Stack constructor
 
-        internal class StackNode<T> // Internal class for the nodes of the stack
+        class StackNode<T> // Internal class for the nodes of the stack
         {
-            T Value;
+            public T Value { get; set; }            // Data stored in the node
+            public StackNode<T> Next { get; set; }    // Reference to the next node
 
             public StackNode(T value)   // creates a node with the given value
             {
                 Value = value;
+                Next = null;
             } // StackNode constructor
 
         } // StackNode nested class
+
+        // Count provides access to the number of elements in the stack
+        public int Count { get => count; set => count = value; }
+
+        // Push adds an element to the top
+        internal void Push(T value)
+        {
+            // Create a new node with the input value
+            StackNode<T> newNode = new StackNode<T>(value);
+
+            if (newNode != null)
+            {
+                // Create a temporary node to keep track of the current top node
+                StackNode<T> current = top;
+
+                // Update the references of the new node so that the current top follows the new node
+                newNode.Next = current;
+            }
+
+            // Assign the new node to be the top
+            top = newNode;
+
+            // increment count to reflect added element
+            count++;
+
+        } // Push
+
+        // Pop removes and returns the element from the top
+        public T Pop()
+        {
+            // If top is null, return default
+            if(top == null)
+            {
+                return default(T);
+            }
+
+            // Assign value to be removed from top to a temporary variable
+            T temp = top.Value;
+
+            // If the next node is empty, reset top to null
+            if (top.Next == null) top = null;
+
+            // If the next node is not null, update reference so it is the new top
+            else if (top.Next != null)
+            {
+                top = top.Next;
+            }
+
+            // decrement the count
+            count--;
+
+            // return the value that was removed
+            return temp;
+        } // Pop
+        
+        // Peek returns the value of the top without removing it
+        public T Peek()
+        {
+            // If top is null, return default
+            if (top == null)
+            {
+                return default(T);
+            }
+
+            // return the value of the top
+            return top.Value;
+        } // Peek
+
+        public void Clear()
+        {
+            // runs until the stack is empty
+            while(top != null)
+            {
+                // If the next node is empty, reset top to null
+                if (top.Next == null) top = null;
+
+                // If the next node is not null, update reference so it is the new top
+                else if (top.Next != null)
+                {
+                    top = top.Next;
+                }
+
+                // decrement the count
+                count--;
+            }
+        } // Clear
 
     } // Stack class
 }
